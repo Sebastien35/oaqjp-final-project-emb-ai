f196ecced2e5d61041d04b2c9a6af5134180c34a
Sebastien35 <sborg,e@live.fr>
2023-08-16 09:10:25 -0400

    v2 code

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,14 +1,26 @@
+
 import requests
 
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    payload = {'text': text_to_analyze}
-    
-    response = requests.post(url, json=payload)
-    
-    if response.status_code == 200:
-        response_data = response.json()
-        return response_data.get('text', '')
-    else:
-        print(f"Error: Request failed with status code {response.status_code}")
-        return None
+import json
+
+def emotion_detector(text_to_analyze):    
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'    
+    myobj = { "raw_document": { "text": text_to_analyze } }    
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}    
+    response = requests.post(url, json=myobj, headers=header)    
+    formatted_response = json.loads(response.text)    
+    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']    
+    anger_score = emotion_predictions['anger']    
+    disgust_score = emotion_predictions['disgust']    
+    fear_score = emotion_predictions['fear']    
+    joy_score = emotion_predictions['joy']    
+    sadness_score = emotion_predictions['sadness']        
+    dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])    
+    return {        
+        'anger': anger_score,        
+        'disgust': disgust_score,        
+        'fear': fear_score,        
+        'joy': joy_score,        
+        'sadness': sadness_score,        
+        'dominant_emotion': dominant_emotion[0]      
+        }  
